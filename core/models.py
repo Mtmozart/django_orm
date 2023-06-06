401f8af3ce4c7de376545856d9f6fe8f5ae37ca3
TheSamuraiBr <mmsnborges@gmail.com>
2023-06-06 15:29:21 -0300

    muito para muitos de vdd

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Chassi(models.Model):
     numero = models.CharField('Chassi', max_length=16, help_text='Máximo 16 caracteres')
@@ -25,9 +26,13 @@ class Carro(models.Model):
     """
     #OneToOneField -- Cada carro poderá ter somente um chassi o chassi com um carro, não pode repetir.
 
-    #ForeingKey -- Cada carro terá uma montadora, mas a montadora pode ter vários carros."""
+    #ForeingKey -- Cada carro terá uma montadora, mas a montadora pode ter vários carros.
+    #ManyToMany - um carro pode ser dirigido por vários motoristas e o motorista pode dirigir vários carros.
+    Não ao mesmo tempo. Pegação geral.
+    """
     chassi = models.OneToOneField(Chassi, on_delete=models.CASCADE)
     montadora = models.ForeignKey(Montadora, on_delete=models.CASCADE)
+    motoristas = models.ManyToManyField(get_user_model())
     modelo = models.CharField('Modelo', max_length=30,  help_text='Máximo 30 caracteres')
     preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
 
@@ -37,6 +42,7 @@ class Carro(models.Model):
 
     def __str__(self):
         return f'Modelo: {self.modelo}, Montadora: {self.montadora}'
+
 
 
 
