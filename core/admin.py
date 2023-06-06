484ee5c148a14cc5ec7138383d1fabcd57e56862
TheSamuraiBr <mmsnborges@gmail.com>
2023-06-06 15:10:15 -0300

    muito para muitos

--- core/admin.py
+++ core/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Chassi, Carro
+from .models import Chassi, Carro, Montadora
 
 @admin.register(Chassi)
 class ChassiAdmin(admin.ModelAdmin):
     list_display = ('numero',)
 
+@admin.register(Montadora)
+class MontadoraAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
 
 @admin.register(Carro)
 class CarroAdmin(admin.ModelAdmin):
-    list_display = ('modelo', 'chassi', 'preco')
+    list_display = ('modelo', 'montadora','chassi', 'preco')
 
 
--- core/models.py
+++ core/models.py
@@ -10,8 +10,24 @@ class Chassi(models.Model):
     def __str__(self):
         return self.numero
 
+class Montadora(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+
+    class Meta:
+        verbose_name = 'Montadora'
+        verbose_name_plural = 'Montadoras'
+
+    def __str__(self):
+        return self.nome
+
+
 class Carro(models.Model):
+    """
+    #OneToOneField -- Cada carro poderá ter somente um chassi o chassi com um carro, não pode repetir.
+
+    #ForeingKey -- Cada carro terá uma montadora, mas a montadora pode ter vários carros."""
     chassi = models.OneToOneField(Chassi, on_delete=models.CASCADE)
+    montadora = models.ForeignKey(Montadora, on_delete=models.CASCADE)
     modelo = models.CharField('Modelo', max_length=30,  help_text='Máximo 30 caracteres')
     preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
 
@@ -20,7 +36,7 @@ class Carro(models.Model):
         verbose_name_plural = 'Carros'
 
     def __str__(self):
-        return self.modelo
+        return f'Modelo: {self.modelo}, Montadora: {self.montadora}'
 
 
 
